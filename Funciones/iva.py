af2a4b06360a60d3587a9f3a3c82e16bfd2341d4
IZebaZI <zebaz.rg@outlook.com>
2024-02-01 22:23:25 -0600

    Correcciones Ejercicio 1

--- Funciones/iva.py
+++ Funciones/iva.py
@@ -1,12 +1,16 @@
 #Ejercicio 1
 def calcularIVA(cantidad, porcentaje = 21):
     resultado = cantidad*(float(porcentaje)/100)
-    print(resultado)
+    return resultado
 
 cantidad = float(input("Ingresa una cantidad: "))
 porcentaje = input("Ingresa un porcentaje (opcional): ")
 
 if porcentaje != "":
-    calcularIVA(cantidad, porcentaje)
+    iva = calcularIVA(cantidad, porcentaje)
+    total = cantidad + iva
+    print(total)
 else:
-    calcularIVA(cantidad)
+    iva = calcularIVA(cantidad)
+    total = cantidad + iva
+    print(total)
